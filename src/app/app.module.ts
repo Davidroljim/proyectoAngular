9eb23985e9728c4f35ebec66ee97ba18be29e58c
davidroljim <david.roldan.jimenez.al@iespoligonosur.org>
2022-02-15 12:28:56 +0100

    clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { AlumnadoDAWComponent } from './alumnado-daw/alumnado-daw.component';
+import { CocheComponent } from './coche/coche.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoComponent,
-    AlumnadoDAWComponent
+    AlumnadoDAWComponent,
+    CocheComponent,
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
